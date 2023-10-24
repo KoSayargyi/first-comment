75d413a792f7695c33ad599aff2a14cd6fec2b6e
first comment <swanhtetaung308@gmail.com>
2023-10-24 18:55:18 +0630

    first commit

---
+++ src/test/java/com/github/spboot/sayargyi/SayargyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spboot.sayargyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayargyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
